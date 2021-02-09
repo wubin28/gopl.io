1d7d5f07acb5603bccc579b8cd167d71e4571d6a
Ben Wu <wubinben@icloud.com>
2021-02-09 09:52:39 +0800

    finish exercise 1.4

--- ch1/dup2/main.go
+++ ch1/dup2/main.go
@@ -15,10 +15,9 @@ import (
 )
 
 func main() {
-	counts := make(map[string]int)
 	files := os.Args[1:]
 	if len(files) == 0 {
-		countLines(os.Stdin, counts)
+		countLines(os.Stdin)
 	} else {
 		for _, arg := range files {
 			f, err := os.Open(arg)
@@ -26,21 +25,23 @@ func main() {
 				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
 				continue
 			}
-			countLines(f, counts)
+			countLines(f)
 			f.Close()
-		}
-	}
-	for line, n := range counts {
-		if n > 1 {
-			fmt.Printf("%d\t%s\n", n, line)
 		}
 	}
 }
 
-func countLines(f *os.File, counts map[string]int) {
+func countLines(f *os.File) {
+	counts := make(map[string]int)
 	input := bufio.NewScanner(f)
 	for input.Scan() {
 		counts[input.Text()]++
+	}
+	fmt.Println(f.Name())
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
 	}
 	// NOTE: ignoring potential errors from input.Err()
 }
