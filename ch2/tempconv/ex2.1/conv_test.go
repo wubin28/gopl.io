a0509ade97ca18f23f2e51474b07bc02bbde6926
Ben Wu <wubinben@icloud.com>
2021-02-12 21:22:56 +0800

    finish TestCToF in ex2.1

---
+++ ch2/tempconv/ex2.1/conv_test.go
@@ -0,0 +1,52 @@
+package tempconv
+
+import (
+	"math"
+	"testing"
+)
+
+func TestCToF(t *testing.T) {
+	type args struct {
+		c Celsius
+	}
+	tests := []struct {
+		name string
+		args args
+		want Fahrenheit
+	}{
+		// TODO: Add test cases.
+		{"Freezing point of water", args{0}, 32},
+		{"Water boils", args{100}, 212},
+		{"Hot bath", args{40}, 104},
+		{"Very cold day", args{-17.78}, 0},
+		{"Extremely cold day", args{-40}, -40},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			esp := 0.01 // acceptable floating point error
+			if got := CToF(tt.args.c); math.Abs(float64(got-tt.want)) > esp {
+				t.Errorf("CToF(%v) = %v, want %v", tt.args.c, got, tt.want)
+			}
+		})
+	}
+}
+
+func TestFToC(t *testing.T) {
+	type args struct {
+		f Fahrenheit
+	}
+	tests := []struct {
+		name string
+		args args
+		want Celsius
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := FToC(tt.args.f); got != tt.want {
+				t.Errorf("FToC() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
